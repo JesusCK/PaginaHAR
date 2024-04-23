f0aeb10b769ffb27fe24cb212429c0a32d3aad55
JesusCK <quirozdj@uninorte.edu.co>
2024-04-23 11:13:53 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, jsonify
 import os
-
 app = Flask(__name__)
 
 actions = []
@@ -16,22 +15,40 @@ def allowed_file(filename):
 def get_actions():
     # Devuelve las últimas dos acciones como JSON
     return jsonify({'actions': actions[-2:]})
+import mysql.connector
+
+# Create a connection to the database
+db = mysql.connector.connect(
+    host="humandetectdb.col4pixfadqv.us-east-2.rds.amazonaws.com",
+    user="admin",
+    password="123456789",
+    database="HumanActionsRecognitions"
+)
+
+cursor = db.cursor()
 
 @app.route('/receive_data', methods=['POST'])
 def receive_data():
     if 'file' in request.files:
-        # Manejar la recepción de archivos GIF
+        # Handle the receipt of GIF files
         file = request.files['file']
         file.save('static/received.gif')
-        return 'Archivo GIF recibido correctamente.'
+        return 'GIF file received correctly.'
     elif request.json and 'action' in request.json and 'date' in request.json:
-        # Manejar la recepción de acciones predichas en formato JSON
+        # Handle the receipt of predicted actions in JSON format
         action = request.json['action']
         date = request.json['date']
         actions.append({'action': action, 'date': date})
-        return 'Acción y fecha recibidas correctamente.'
+
+        # Insert the action and date into the database
+        query = "INSERT INTO registro (fecha, accion) VALUES (%s, %s)"
+        values = (date, action)
+        cursor.execute(query, values)
+        db.commit()
+
+        return 'Action and date received correctly.'
     else:
-        return 'Solicitud incorrecta.', 400
+        return 'Incorrect request.', 400
     
 
 @app.route('/')
