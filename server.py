74345f5f2c108dd2dbd116d57dd86481519e25d9
JesusCK <quirozdj@uninorte.edu.co>
2024-05-13 14:54:34 -0500

    Update server.py

--- server.py
+++ server.py
@@ -124,7 +124,7 @@ def prueba():
 def consultar_historicos():
     if request.method == 'POST':
         # Obtener las fechas de inicio y fin del formulario
-        fecha = request.form
+        fecha = request.form.get('fecha')
         accion = request.form.get('accion')
         print(fecha)  # Obtener la acción seleccionada
         
